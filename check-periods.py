0c25a429a2d46112e1e1b06e5b663dbc2c44ee41
Oliver Greenwood <greenwood672.dev@gmail.com>
2024-09-04 11:14:09 +0100

    runnoing multiple degree/power

--- check-periods.py
+++ check-periods.py
@@ -5,13 +5,14 @@ from matplotlib import pyplot as plt
 directory = './lr_data'
 
 for filename in sorted(os.listdir(directory)):
+    filename = "-1729228464322526488-period-0.csv"
     if filename.endswith('.csv'):
         file_path = os.path.join(directory, filename)
         
         df = pd.read_csv(file_path)
         
         plt.figure(figsize=(10, 6))
-        plt.plot(df['timestamps'], df['aAcc'], label='Acceleration')
+        plt.plot(df['actual_timestamps'], df['aAcc'], label='Acceleration')
         
         # Set the title to the filename (without the directory path)
         plt.title(f'{filename}')
@@ -26,3 +27,9 @@ for filename in sorted(os.listdir(directory)):
         # Show the plot
         plt.show()
         
+
+"""
+-1206614467121466218
+-1729228464322526488
+
+"""
