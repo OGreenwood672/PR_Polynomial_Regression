077b20120be7b82249ce7e1759d190495302e633
Oliver Greenwood <greenwood672.dev@gmail.com>
2024-09-09 16:58:19 +0100

    Got dynamic ideal kinematics working

--- check-periods.py
+++ check-periods.py
@@ -5,20 +5,20 @@ from matplotlib import pyplot as plt
 directory = './lr_data'
 
 for filename in sorted(os.listdir(directory)):
-    if filename.endswith('.csv'):
+    if filename.endswith('.csv'):# and "-8275661513257109100" in filename:
         file_path = os.path.join(directory, filename)
         
         df = pd.read_csv(file_path)
         
         plt.figure(figsize=(10, 6))
-        plt.plot(df['actual_timestamps'], df['aHeight'], label='Height')
+        plt.plot(df['timestamps'], df['aAcc'], label='Acc')
         
         # Set the title to the filename (without the directory path)
         plt.title(f'{filename}')
         
         # Label the axes
         plt.xlabel('Timestamps')
-        plt.ylabel('Height')
+        plt.ylabel('Acceleration')
         
         # Display the legend
         plt.legend()
